7d418f52fbacfcdafa0df4e145e71fd56f5489e4
seekforwings <378995013@qq.com>
2018-02-10 23:12:16 +0800

    add dev_func1

--- simplecpp/simplecpp/simplecpp/simplecpp.cpp
+++ simplecpp/simplecpp/simplecpp/simplecpp.cpp
@@ -3,11 +3,17 @@
 
 #include "stdafx.h"
 
+void dev_func1() {
+	printf("dev_func1");
+}
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("hello world\n");
 	printf("hello 5");
+
+	dev_func1();
+
 	return 0;
 }
 
