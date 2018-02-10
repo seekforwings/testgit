ca51de863725bde0787ff417f353de399fddb8fc
seekforwings <378995013@qq.com>
2018-02-10 23:19:11 +0800

    add main_func1

--- simplecpp/simplecpp/simplecpp/simplecpp.cpp
+++ simplecpp/simplecpp/simplecpp/simplecpp.cpp
@@ -3,11 +3,18 @@
 
 #include "stdafx.h"
 
+void main_fun1() {
+	printf("main_func1");
+}
+
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	printf("hello world\n");
 	printf("hello 5");
+
+	main_fun1();
+
 	return 0;
 }
 
